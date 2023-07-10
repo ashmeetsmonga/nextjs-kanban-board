e2eb9934bf8798d902f00e172c848ec8acdbcf2d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-10 21:37:59 +0530

    added delete functionality

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+import { AiFillDelete } from "react-icons/ai";
+import { useTaskStore } from "../store/useTaskStore";
 
 interface TaskProps {
 	title: string;
@@ -7,10 +11,18 @@ interface TaskProps {
 }
 
 const Task: React.FC<TaskProps> = ({ title, description, color }) => {
+	const deleteTask = useTaskStore((state) => state.deleteTask);
+
 	return (
-		<div className={`w-full rounded bg-[${color}] p-4`}>
+		<div className={`w-full flex flex-col gap-1 rounded bg-[${color}] p-4`}>
 			<h3 className='font-semibold text-xl'>{title}</h3>
 			<p className='font-light'>{description}</p>
+
+			<AiFillDelete
+				onClick={() => deleteTask(title)}
+				className='ml-auto cursor-pointer'
+				size={25}
+			/>
 		</div>
 	);
 };
--- app/store/useTaskStore.ts
+++ app/store/useTaskStore.ts
@@ -6,6 +6,7 @@ interface TaskStoreProps {
 	closeModal: () => void;
 	tasks: Task[];
 	addTask: (title: string, description: string, state: string, color: string) => void;
+	deleteTask: (title: string) => void;
 }
 
 export const useTaskStore = create<TaskStoreProps>((set) => ({
@@ -15,4 +16,6 @@ export const useTaskStore = create<TaskStoreProps>((set) => ({
 	tasks: [],
 	addTask: (title: string, description: string, state: string, color: string) =>
 		set((prevState) => ({ tasks: [...prevState.tasks, { title, description, state, color }] })),
+	deleteTask: (title: string) =>
+		set((state) => ({ tasks: state.tasks.filter((task) => task.title !== title) })),
 }));
