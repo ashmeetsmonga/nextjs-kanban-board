c09cb91594ebe0c40cd48d861e02b5ceda8e943e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-11 00:26:48 +0530

    added dashed border

--- app/components/Board.tsx
+++ app/components/Board.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useTaskStore } from "../store/useTaskStore";
 import Task from "./Task";
 
@@ -14,21 +14,33 @@ const Board: React.FC<BoardProps> = ({ state }) => {
 	const setDraggedTask = useTaskStore((state) => state.setDraggedTask);
 	const moveTask = useTaskStore((state) => state.moveTask);
 
+	const [drop, setDrop] = useState(false);
+
 	const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
+		setDrop(true);
 	};
 
 	const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
 		moveTask(draggedTask, state);
 		setDraggedTask(null);
+		setDrop(false);
+	};
+
+	const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+		e.preventDefault();
+		setDrop(false);
 	};
 
 	return (
 		<div
 			onDragOver={onDragOver}
 			onDrop={onDrop}
-			className='bg-[#262626] w-1/3 max-w-[400px] min-h-[500px] text-white p-4 rounded'
+			onDragLeave={onDragLeave}
+			className={`bg-[#262626] w-1/3 max-w-[400px] min-h-[500px] text-white p-4 rounded ${
+				drop ? "border-2 border-gray-300 border-dashed" : "border-2 border-transparent"
+			}`}
 		>
 			<h2 className='text-2xl text-white font-bold capitalize'>{state}</h2>
 			<div className='w-full flex flex-col gap-2 mt-5'>
