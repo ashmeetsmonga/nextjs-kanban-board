7e855a3e76741a8bd37707cf54e7ffc0375fa5c7
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-11 13:58:05 +0530

    added persist data and hydration error check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={`${inter.className} overflow-x-hidden`}>{children}</body>
+			<body className={`${inter.className} overflow-x-hidden`}>
+				<ClientOnly>{children}</ClientOnly>
+			</body>
 		</html>
 	);
 }
--- app/store/useTaskStore.ts
+++ app/store/useTaskStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface TaskStoreProps {
 	isModalOpen: boolean;
@@ -12,21 +13,28 @@ interface TaskStoreProps {
 	moveTask: (title: string | null, state: string) => void;
 }
 
-export const useTaskStore = create<TaskStoreProps>((set) => ({
-	isModalOpen: false,
-	openModal: () => set((prevState) => ({ ...prevState, isModalOpen: true })),
-	closeModal: () => set((prevState) => ({ ...prevState, isModalOpen: false })),
-	tasks: [],
-	draggedTask: null,
-	addTask: (title: string, description: string, state: string, color: string) =>
-		set((prevState) => ({ tasks: [...prevState.tasks, { title, description, state, color }] })),
-	deleteTask: (title: string) =>
-		set((state) => ({ tasks: state.tasks.filter((task) => task.title !== title) })),
-	setDraggedTask: (title: string | null) => set((state) => ({ draggedTask: title })),
-	moveTask: (title: string | null, state: string) =>
-		set((prevState) => ({
-			tasks: prevState.tasks.map((task) =>
-				task.title === title ? { ...task, title, state } : task
-			),
-		})),
-}));
+export const useTaskStore = create<TaskStoreProps>()(
+	persist(
+		(set) => ({
+			isModalOpen: false,
+			openModal: () => set((prevState) => ({ ...prevState, isModalOpen: true })),
+			closeModal: () => set((prevState) => ({ ...prevState, isModalOpen: false })),
+			tasks: [],
+			draggedTask: null,
+			addTask: (title: string, description: string, state: string, color: string) =>
+				set((prevState) => ({ tasks: [...prevState.tasks, { title, description, state, color }] })),
+			deleteTask: (title: string) =>
+				set((state) => ({ tasks: state.tasks.filter((task) => task.title !== title) })),
+			setDraggedTask: (title: string | null) => set((state) => ({ draggedTask: title })),
+			moveTask: (title: string | null, state: string) =>
+				set((prevState) => ({
+					tasks: prevState.tasks.map((task) =>
+						task.title === title ? { ...task, title, state } : task
+					),
+				})),
+		}),
+		{
+			name: "kanban-board",
+		}
+	)
+);
