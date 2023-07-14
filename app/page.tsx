1a20c9da23f2cee6352e1e28d4655dee9aedc247
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-14 17:48:15 +0530

    saving tasks into indexeddb added

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,40 @@ import Board from "./components/Board";
 import Modal from "./components/Modal";
 import EditModal from "./components/EditModal";
 import { AiFillGithub } from "react-icons/ai";
+import { set } from "idb-keyval";
 
 import { useModalStore } from "./store/useModalStore";
+import { useTaskStore } from "./store/useTaskStore";
+import { toast } from "react-hot-toast";
 
 export default function Home() {
 	const openModal = useModalStore((state) => state.openModal);
+	const tasks = useTaskStore((state) => state.tasks);
+
+	const saveSnapshot = () => {
+		let toastID = toast.loading("Saving snapshot");
+		const date = new Date();
+		const key = `${date.getDate()}|${date.getMonth()}|${date.getFullYear()}`;
+		set(key, JSON.stringify(tasks))
+			.then(() => toast.success("Snapshot saved", { id: toastID }))
+			.catch(() => toast.error("Error in saving snapshot", { id: toastID }));
+	};
 
 	return (
 		<main className='relative flex w-screen min-h-screen flex-col items-center px-10 py-10 bg-black'>
 			<div className='w-full flex justify-between self-start'>
 				<h1 className='text-6xl text-white font-semibold'>Kanban Board</h1>
-				<button
-					onClick={openModal}
-					className='bg-[#262626] py-2 px-5 flex justify-center items-center rounded-full text-3xl text-white'
-				>
-					+
-				</button>
+				<div className='flex gap-4'>
+					<button
+						onClick={openModal}
+						className='bg-[#262626] py-2 px-5 flex justify-center items-center rounded-full text-3xl text-white'
+					>
+						+
+					</button>
+					<button onClick={saveSnapshot} className='bg-[#262626] px-4 py-2 text-white rounded-xl'>
+						Save Snapshot
+					</button>
+				</div>
 			</div>
 			<div className='self-start flex gap-2 items-center'>
 				<p className='text-white'>Created by Ashmeet Singh Monga</p>
